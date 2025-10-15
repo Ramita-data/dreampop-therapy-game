f5cd12a346efcc8fec25ad132bbc48ca030624e0
Ramita-data <ramitagouda6@gmail.com>
2025-10-15 18:52:51 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,30 @@ const cacheName = 'dreampop-cache-v1';
 const assets = [
   'index.html',
   'manifest.json',
-  'style.css',        // optional, if you create CSS separately
-  'app.js',           // optional, if you create JS separately
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png'
+  'style.css',  // optional if you have one
+  'app.js',     // optional if you have one
+  'icon-192.png',
+  'icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key!==cacheName).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))
+    )
+  );
 });
+
+
